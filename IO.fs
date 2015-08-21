e955abae7e73a351d247e5b38621d6de451383c3
neo <liangneo@qq.com>
2015-08-21 22:55:08 +0800

    add simple io

---
+++ IO.fs
@@ -0,0 +1,16 @@
+﻿
+
+namespace fjson
+open System
+open System.IO
+open fjson.parser
+open fjson.render
+module SimpleIO =
+    let readFile (f:string) = 
+        use sr = new StreamReader(f)
+        let content = sr.ReadToEnd()
+        formmater.read content
+    let writeFile (v:JValue) (f:string) =
+        let str = simple v
+        use sw = new StreamWriter(f)
+        sw.Write str
